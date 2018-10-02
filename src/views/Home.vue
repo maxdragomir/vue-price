f35a5336990928e0d8cfd95a86216d682a160aef
BoBRoID <n.gilko@gmail.com>
2018-10-03 02:45:13 +0300

    try to revert some worst

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,92 +1,105 @@
-<template>
+<template >
   <div>
-    <section class="hero has-text-centered">
+
+    <section class="hero is-medium has-text-centered is-header">
       <div class="hero-body">
-        <div class="container">
-          <h1 class="title">
-            Price Comparer
-          </h1>
-          <h2 class="subtitle">
-            Here you can compare price for products in some shops
-          </h2>
-        </div>
+          <div class="container">
+              <div class="svg">
+                  <img src="../assets/img/market.svg" alt="">
+              </div>
+              <h1 class="title">Price Comparer</h1>
+              <h2 class="subtitle">Here you can compare price for products in some shops</h2>
+          </div>
       </div>
-      <div class="box">
-        <div class="container">
-          <div class="columns">
-            <div class="column is-three-fifths-desktop is-offset-one-fifth-desktop">
-              <nav class="level">
-                <div class="level-item has-text-centered">
-                  <div>
-                    <p class="title">We have...</p>
-                  </div>
+        <div class="is-home-numbers">
+            <div class="container">
+                <div class="columns is-gapless">
+                    <div class="column is-two-thirds has-text-centered">
+                        <div class="level">
+                            <div class="level-item has-text-centered">
+                                <div><p class="title">We have:</p></div>
+                            </div>
+                            <div class="level-item has-text-centered is-products-stats">
+                                <div>
+                                    <p class="title">{{ stats.products }}</p>
+                                    <p class="heading">Products</p>
+                                    <div class="svg">
+                                        <img src="../assets/img/shopping-bag.svg" alt="">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="level-item has-text-centered">
+                                <div>
+                                    <p class="title">{{ stats.categories }}</p>
+                                    <p class="heading">Categories</p>
+                                    <div class="svg">
+                                        <img src="../assets/img/checklist.svg" alt="">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="level-item has-text-centered">
+                                <div>
+                                    <p class="title">{{ stats.shops }}</p>
+                                    <p class="heading">Shops</p>
+                                    <div class="svg">
+                                        <img src="../assets/img/shop.svg" alt="">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div class="level-item has-text-centered">
-                  <div>
-                    <p class="title">{{ stats.products }}</p>
-                    <p class="heading">Products</p>
-                  </div>
-                </div>
-                <div class="level-item has-text-centered">
-                  <div>
-                    <p class="title">{{ stats.categories }}</p>
-                    <p class="heading">Categories</p>
-                  </div>
-                </div>
-                <div class="level-item has-text-centered">
-                  <div>
-                    <p class="title">{{ stats.shops }}</p>
-                    <p class="heading">Shops</p>
-                  </div>
-                </div>
-              </nav>
             </div>
-          </div>
         </div>
+    </section>
+
+    <section class="section is-try">
+      <div class="container">
+          <div class="has-text-centered">
+              <h1 class="title">Try it yourself!</h1>
+              <h2 class="subtitle">just start typing name of product or barcode</h2>
+          </div>
+          <div class="columns">
+              <div class="column is-half-desktop is-offset-one-quarter-desktop">
+                  <b-field class="has-t-margin-2 is-home-input">
+                      <b-input v-model="searchQuery" placeholder="Name or barcode of product" icon="search" v-bind:class="{ 'no-result': noResult}"></b-input>
+                  </b-field>
+              </div>
+          </div>
       </div>
     </section>
 
-    <section class="section">
+    <section class="section is-products has-t-padding-0 has-b-padding-5">
       <div class="container">
-        <div class="has-text-centered">
-          <h1 class="title">Try it yourself!</h1>
-          <h2 class="subtitle">
-            just start typing name of product or barcode
-          </h2>
-        </div>
-        <div class="columns">
-          <div class="column is-half-desktop is-offset-one-quarter-desktop">
-            <b-field class="has-v-margin-1">
-              <b-input v-model="searchQuery" placeholder="Name or barcode of product" icon="search"></b-input>
-            </b-field>
-          </div>
-        </div>
-      </div>
-    </section>
 
-    <section class="section">
-      <div class="container">
           <section class="searchResults" v-if="isSearchUsed">
-              <h2>We've found this products:</h2>
-              <div class="columns">
-                  <div class="column" v-for="product in searchResults" :key="product.id">
+              <h2 v-if="noResult">No result</h2>
+              <h2 v-else>We've found this products:</h2>
+              <div class="columns is-multiline is-two-thirds-mobile">
+                  <div class="column is-one-third-tablet is-one-fifth-desktop" v-for="product in searchResults" :key="product.id">
                       <ProductCard v-bind:product="product" />
                   </div>
               </div>
           </section>
+
           <section class="latestProducts" v-else>
-              <div class="columns">
-                  <div class="column" v-for="product in latestProducts" :key="product.id">
+              <h2>Last products:</h2>
+              <div class="columns is-multiline is-two-thirds-mobile">
+                  <div class="column is-one-third-tablet is-one-fifth-desktop" v-for="product in latestProducts" :key="product.id">
                       <ProductCard v-bind:product="product" />
                   </div>
               </div>
           </section>
+
       </div>
     </section>
+
   </div>
+
 </template>
 
 <script>
+
   import gql from 'graphql-tag'
   import ProductCard from "../components/ProductCard";
 
@@ -99,7 +112,7 @@
   }`;
 
   const latestProducts = gql`query{
-    products(count: 5, orderBy: updated, orderDirection: desc){
+    products(count: 10, orderBy: updated, orderDirection: desc){
       id
       name
       categoriesIds
